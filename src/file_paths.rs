37d65cc05061a4f69e8f192a7d34b978a758127f
Jamie Mason <jamie@foldleft.io>
2019-08-08 15:53:47 +0100

    refactor: reduce nesting

--- src/file_paths.rs
+++ src/file_paths.rs
@@ -1,15 +1,14 @@
 pub fn get_paths(pattern: &str) -> Vec<String> {
-  let mut paths: Vec<String> = vec![];
-  for entry in glob::glob(pattern).expect("Failed to read glob pattern") {
-    match entry {
-      Err(e) => println!("{:?}", e),
-      Ok(path) => match path.to_str() {
-        None => panic!("new path is not a valid UTF-8 sequence"),
-        Some(path_str) => {
-          paths.push(String::from(path_str));
-        }
-      },
-    };
+  let mut file_paths: Vec<String> = vec![];
+  let glob_paths = glob::glob(pattern).expect("Failed to read glob pattern");
+  for glob_path in glob_paths {
+    let path_buffer = glob_path.expect("Failed to unwrap glob entry");
+    match path_buffer.to_str() {
+      None => panic!("New path is not a valid UTF-8 sequence"),
+      Some(path_str) => {
+        file_paths.push(String::from(path_str));
+      }
+    }
   }
-  paths
+  file_paths
 }
