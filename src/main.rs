4fc244ffd03885396f8883d185df28c9a7989b04
Jamie Mason <jamie@foldleft.io>
2023-12-30 22:26:23 +0000

    [WIP]

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,23 @@ mod package_json;
 use std::io;
 
 fn main() -> io::Result<()> {
-  let pattern = "/Users/foldleft/Dev/FluidFramework/packages/*/*/package.json";
+  // let pattern = "/Users/foldleft/Dev/FluidFramework/packages/*/*/package.json";
+  let pattern = "/Users/foldleft/Dev/FluidFramework/package.json";
   let paths = file_paths::get_paths(pattern);
 
   println!("PATHS: {:?}", paths);
 
   paths.into_iter().try_for_each(|path| {
-    let package = package_json::get_package(path)?;
-    dependencies::list_dependencies(&package);
+    match package_json::read_and_parse_json(path) {
+      Ok(mut json) => {
+        // Example of mutating the JSON object
+        json["name"] = serde_json::Value::from("NewName");
+        println!("Updated JSON: {:?}", json);
+      }
+      Err(e) => println!("Error reading file: {}", e),
+    }
+    // let package = package_json::get_package(path)?;
+    // dependencies::list_dependencies(&package);
     Ok(())
   })
 }
--- src/package_json.rs
+++ src/package_json.rs
@@ -1,10 +1,17 @@
-use serde_json::Value as JsonValue;
+use serde_json::Value;
 use std::fs;
 use std::io;
+use std::path::Path;
+
+pub fn read_and_parse_json<P: AsRef<Path>>(path: P) -> io::Result<Value> {
+  let file_contents = fs::read_to_string(path)?;
+  let parsed_json: Value = serde_json::from_str(&file_contents)?;
+  Ok(parsed_json)
+}
 
 #[derive(Debug)]
 pub struct Package {
-  pub data: JsonValue,
+  pub data: Value,
   pub json: String,
   pub path: String,
 }
