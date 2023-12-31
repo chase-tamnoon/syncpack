c172655dc353fcc0da96cdb0116cdd8e1833771c
Jamie Mason <jamie@foldleft.io>
2023-12-31 11:38:03 +0000

    [WIP]

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,38 @@
+// Standard library imports
+use std::{io, path::Path};
+
+// External crates
 extern crate glob;
 extern crate serde;
 extern crate serde_json;
 
+// Local modules
 mod dependencies;
 mod file_paths;
 mod package_json;
 
-use std::{io, path::Path};
+// Imports from external crates
 use serde_json::json;
 
 fn main() -> io::Result<()> {
-  // let pattern = "/Users/foldleft/Dev/FluidFramework/packages/*/*/package.json";
   let pattern = "/Users/foldleft/Dev/FluidFramework/package.json";
   let paths = file_paths::get_paths(pattern);
 
   println!("PATHS: {:?}", paths);
 
-  paths.into_iter().try_for_each(|file_path| {
-    match package_json::read_file(&file_path) {
-      Ok(mut package) => {
-        let file_path: &Path = package.file_path();
-        println!("file_path: {:?}", file_path);
+  paths
+    .into_iter()
+    .map(|file_path| package_json::read_file(&file_path))
+    .filter_map(Result::ok)
+    .for_each(|mut package| {
+      let file_path: &Path = package.file_path();
+      println!("file_path: {:?}", file_path);
 
-        // Example of mutating the JSON object
-        if let Some(name) = package.contents.pointer_mut("/name") {
-          *name = json!("new value");
-        }
-        println!("Updated JSON: {:?}", package);
+      if let Some(name) = package.contents.pointer_mut("/name") {
+        *name = json!("new value");
       }
-      Err(e) => println!("Error reading file: {}", e),
-    }
-    // let package = package_json::get_package(path)?;
-    // dependencies::list_dependencies(&package);
-    Ok(())
-  })
+      println!("Updated JSON: {:#?}", package.contents);
+    });
+
+  Ok(())
 }
