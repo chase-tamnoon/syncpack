9beba353c28f965f8037dbb729df18683de9dbe0
Jamie Mason <jamie@foldleft.io>
2023-12-31 21:26:59 +0000

    trying to sort object

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,7 @@ mod file_paths;
 mod package_json;
 
 // Imports from external crates
-use serde_json::json;
-use std::collections::BTreeMap;
+use serde_json::{json, Map};
 
 fn main() -> io::Result<()> {
   let pattern = "/Users/foldleft/Dev/FluidFramework/package.json";
@@ -27,20 +26,54 @@ fn main() -> io::Result<()> {
       package.set_prop("/name", json!("new name"));
       package.set_prop("/c8/cache-dir", json!("new cache-dir"));
       sort_json(&mut package.contents);
+      sort_first(
+        &mut package.contents,
+        vec![
+          "private".to_string(),
+          "homepage".to_string(),
+          "name".to_string(),
+          "version".to_string(),
+          "description".to_string(),
+        ],
+      );
       package.pretty_print();
     });
 
   Ok(())
 }
 
+/// Sort the keys in a JSON object, with the given keys first
+fn sort_first(value: &mut serde_json::Value, order: Vec<String>) {
+  match value {
+    serde_json::Value::Object(obj) => {
+      let mut sorted_obj: serde_json::Map<String, serde_json::Value> =
+        serde_json::Map::new();
+      let order_clone = order.clone(); // Clone the order vector
+
+      for key in order_clone {
+        if let Some(val) = obj.remove(&key) {
+          sorted_obj.insert(key.clone(), val);
+        }
+      }
+
+      sorted_obj.extend(obj.iter().map(|(k, v)| (k.clone(), v.clone())));
+
+      *value = serde_json::Value::Object(sorted_obj);
+    }
+    _ => {}
+  }
+}
+
 fn sort_json(value: &mut serde_json::Value) {
   match value {
     serde_json::Value::Object(obj) => {
-      let sorted_obj: BTreeMap<String, serde_json::Value> = obj
-        .into_iter()
-        .map(|(k, v)| (k.clone(), v.clone()))
-        .collect();
-      obj.extend(sorted_obj);
+      let mut entries: Vec<_> =
+        obj.iter().map(|(k, v)| (k.clone(), v.clone())).collect();
+      entries.sort_by(|a, b| a.0.cmp(&b.0));
+      let sorted_obj: Map<String, serde_json::Value> =
+        entries.into_iter().collect();
+
+      *value = serde_json::Value::Object(sorted_obj);
     }
     serde_json::Value::Array(arr) => {
       arr.sort_by(|a, b| {
--- src/package_json.rs
+++ src/package_json.rs
@@ -33,7 +33,7 @@ impl Package {
   }
 }
 
-/// Read and parse a package.json file, returning a Package which can be freely mutated
+/// Read and parse a package.json file
 pub fn read_file<P: AsRef<Path>>(file_path: &P) -> io::Result<Package> {
   let file_contents = fs::read_to_string(file_path)?;
   let parsed_json: Value = serde_json::from_str(&file_contents)?;
