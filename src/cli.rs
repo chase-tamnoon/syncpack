207f2b49e61026a3732e2c29f79f0ec18094696f
Jamie Mason <jamie@foldleft.io>
2024-01-01 16:34:23 +0000

    [WIP]

---
+++ src/cli.rs
@@ -0,0 +1,72 @@
+use clap::{Arg, ArgMatches, Command};
+
+extern crate glob;
+extern crate serde;
+extern crate serde_json;
+
+pub fn create() -> Command {
+  Command::new("syncpack")
+    .about("Consistent dependency versions in large JavaScript Monorepos")
+    .version("13.0.0")
+    .author("Jamie Mason <jamie@foldleft.io> (https://github.com/JamieMason)")
+    .subcommand(
+      Command::new("lint")
+        .about("Lint command")
+        .arg(
+          Arg::new("format")
+            .long("format")
+            .action(clap::ArgAction::SetTrue),
+        )
+        .arg(
+          Arg::new("ranges")
+            .long("ranges")
+            .action(clap::ArgAction::SetTrue),
+        )
+        .arg(
+          Arg::new("versions")
+            .long("versions")
+            .action(clap::ArgAction::SetTrue),
+        ),
+    )
+    .subcommand(
+      Command::new("fix")
+        .about("Fix command")
+        .arg(
+          Arg::new("format")
+            .long("format")
+            .action(clap::ArgAction::SetTrue),
+        )
+        .arg(
+          Arg::new("ranges")
+            .long("ranges")
+            .action(clap::ArgAction::SetTrue),
+        )
+        .arg(
+          Arg::new("versions")
+            .long("versions")
+            .action(clap::ArgAction::SetTrue),
+        ),
+    )
+}
+
+#[derive(Debug)]
+pub struct EnabledSteps {
+  format: bool,
+  ranges: bool,
+  versions: bool,
+}
+
+/// returns which steps to run. if none are true, then all of them are returned
+/// as true. if any of them are true, then only those are returned as true.
+pub fn get_enabled_steps(matches: &ArgMatches) -> EnabledSteps {
+  let use_format = matches.get_flag("format");
+  let use_ranges = matches.get_flag("ranges");
+  let use_versions = matches.get_flag("versions");
+  let use_all = !use_format && !use_ranges && !use_versions;
+
+  EnabledSteps {
+    format: use_all || use_format,
+    ranges: use_all || use_ranges,
+    versions: use_all || use_versions,
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use clap::{arg, Arg, ArgMatches, Command};
 use std::io;
 
 extern crate glob;
 extern crate serde;
 extern crate serde_json;
 
+mod cli;
 mod config;
 mod dependencies;
 mod file_paths;
@@ -12,76 +12,20 @@ mod format;
 mod package_json;
 
 fn main() -> io::Result<()> {
-  let matches = Command::new("syncpack")
-    .about("Consistent dependency versions in large JavaScript Monorepos")
-    .version("13.0.0")
-    .author("Jamie Mason <jamie@foldleft.io> (https://github.com/JamieMason)")
-    .subcommand(
-      Command::new("lint")
-        .about("Lint command")
-        .arg(
-          Arg::new("format")
-            .long("format")
-            .action(clap::ArgAction::SetTrue),
-        )
-        .arg(
-          Arg::new("versions")
-            .long("versions")
-            .action(clap::ArgAction::SetTrue),
-        )
-        .arg(
-          Arg::new("ranges")
-            .long("ranges")
-            .action(clap::ArgAction::SetTrue),
-        ),
-    )
-    .subcommand(
-      Command::new("fix")
-        .about("Fix command")
-        .arg(
-          Arg::new("format")
-            .long("format")
-            .action(clap::ArgAction::SetTrue),
-        )
-        .arg(
-          Arg::new("versions")
-            .long("versions")
-            .action(clap::ArgAction::SetTrue),
-        )
-        .arg(
-          Arg::new("ranges")
-            .long("ranges")
-            .action(clap::ArgAction::SetTrue),
-        ),
-    )
-    .get_matches();
+  let matches = cli::create().get_matches();
 
   // You can then check which subcommand was used and which flags were set
   match matches.subcommand() {
     Some(("lint", lint_matches)) => {
-      println!("lint enabled: {:?}", get_enabled_steps(lint_matches));
+      println!("lint enabled: {:?}", cli::get_enabled_steps(lint_matches));
       Ok(())
     }
     Some(("fix", fix_matches)) => {
-      println!("fix enabled: {:?}", get_enabled_steps(fix_matches));
+      println!("fix enabled: {:?}", cli::get_enabled_steps(fix_matches));
       Ok(())
     }
     _ => Err(create_error("unrecognized subcommand")),
   }
-}
-
-/// returns which steps to run from "format", "versions", "ranges". if none are true, then all of them are returned as true.
-/// if any of them are true, then only those are returned as true.
-fn get_enabled_steps(matches: &ArgMatches) -> (bool, bool, bool) {
-  let format_is_set = matches.get_flag("format");
-  let versions_is_set = matches.get_flag("versions");
-  let ranges_is_set = matches.get_flag("ranges");
-
-  if !format_is_set && !versions_is_set && !ranges_is_set {
-    return (true, true, true);
-  }
-
-  (format_is_set, versions_is_set, ranges_is_set)
 }
 
 fn create_error(message: &str) -> io::Error {
