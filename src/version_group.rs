db3dfbad444b0bb39ea4b2942e0ca0803fb7af20
Jamie Mason <jamie@foldleft.io>
2024-01-02 15:23:04 +0000

    [WIP]

--- src/version_group.rs
+++ src/version_group.rs
@@ -62,3 +62,56 @@ pub struct AnyVersionGroup {
   pub snap_to: Option<Vec<String>>,
   pub prefer_version: Option<String>,
 }
+
+impl AnyVersionGroup {
+  pub fn create(&self, index: usize) -> VersionGroup {
+    if let Some(true) = self.is_banned {
+      return VersionGroup::Banned(BannedVersionGroup {
+        selector: create_selector(self, index),
+        is_banned: true,
+      });
+    }
+    if let Some(true) = self.is_ignored {
+      return VersionGroup::Ignored(IgnoredVersionGroup {
+        selector: create_selector(self, index),
+        is_ignored: true,
+      });
+    }
+    if let Some(pin_version) = &self.pin_version {
+      return VersionGroup::Pinned(PinnedVersionGroup {
+        selector: create_selector(self, index),
+        pin_version: pin_version.clone(),
+      });
+    }
+    if let Some(policy) = &self.policy {
+      return VersionGroup::SameRange(SameRangeVersionGroup {
+        selector: create_selector(self, index),
+        policy: policy.clone(),
+      });
+    }
+    if let Some(snap_to) = &self.snap_to {
+      return VersionGroup::SnappedTo(SnappedToVersionGroup {
+        selector: create_selector(self, index),
+        snap_to: snap_to.clone(),
+      });
+    }
+    if let Some(prefer_version) = &self.prefer_version {
+      return VersionGroup::Standard(StandardVersionGroup {
+        selector: create_selector(self, index),
+        prefer_version: prefer_version.clone(),
+      });
+    }
+    panic!("Invalid version group");
+  }
+}
+
+fn create_selector(group: &AnyVersionGroup, index: usize) -> group_selector::GroupSelector {
+  group_selector::GroupSelector {
+    dependencies: group.dependencies.clone(),
+    dependency_types: group.dependency_types.clone(),
+    label: group.label.clone(),
+    index,
+    packages: group.packages.clone(),
+    specifier_types: group.specifier_types.clone(),
+  }
+}
