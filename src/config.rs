7c87498a52ec540d39264c9e640870980f2857a3
Jamie Mason <jamie@foldleft.io>
2024-01-02 21:21:21 +0000

    [WIP]

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,5 @@
 use serde::Deserialize;
-use std::collections;
+use std::collections::HashMap;
 use std::path;
 
 use crate::file_paths;
@@ -7,22 +7,97 @@ use crate::semver_group;
 use crate::strategy;
 use crate::version_group;
 
+fn empty_custom_types() -> HashMap<String, strategy::AnyStrategy> {
+  HashMap::new()
+}
+
+fn default_true() -> bool {
+  true
+}
+
+fn default_filter() -> String {
+  ".".to_string()
+}
+
+fn default_indent() -> String {
+  "  ".to_string()
+}
+
+fn default_sort_az() -> Vec<String> {
+  vec![
+    "bin".to_string(),
+    "contributors".to_string(),
+    "dependencies".to_string(),
+    "devDependencies".to_string(),
+    "keywords".to_string(),
+    "peerDependencies".to_string(),
+    "resolutions".to_string(),
+    "scripts".to_string(),
+  ]
+}
+
+fn default_sort_exports() -> Vec<String> {
+  vec![
+    "types".to_string(),
+    "node-addons".to_string(),
+    "node".to_string(),
+    "browser".to_string(),
+    "module".to_string(),
+    "import".to_string(),
+    "require".to_string(),
+    "development".to_string(),
+    "production".to_string(),
+    "script".to_string(),
+    "default".to_string(),
+  ]
+}
+
+fn sort_first() -> Vec<String> {
+  vec![
+    "name".to_string(),
+    "description".to_string(),
+    "version".to_string(),
+    "author".to_string(),
+  ]
+}
+
+fn default_source() -> Vec<String> {
+  vec![
+    "package.json".to_string(),
+    "packages/*/package.json".to_string(),
+  ]
+}
+
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Rcfile {
-  pub custom_types: collections::HashMap<String, strategy::AnyStrategy>,
+  #[serde(default = "empty_custom_types")]
+  pub custom_types: HashMap<String, strategy::AnyStrategy>,
+  #[serde(default)]
   pub dependency_types: Vec<String>,
+  #[serde(default = "default_filter")]
   pub filter: String,
+  #[serde(default = "default_true")]
   pub format_bugs: bool,
+  #[serde(default = "default_true")]
   pub format_repository: bool,
+  #[serde(default = "default_indent")]
   pub indent: String,
+  #[serde(default)]
   pub semver_groups: Vec<semver_group::AnySemverGroup>,
+  #[serde(default = "default_sort_az")]
   pub sort_az: Vec<String>,
+  #[serde(default = "default_sort_exports")]
   pub sort_exports: Vec<String>,
+  #[serde(default = "sort_first")]
   pub sort_first: Vec<String>,
+  #[serde(default = "default_true")]
   pub sort_packages: bool,
+  #[serde(default = "default_source")]
   pub source: Vec<String>,
+  #[serde(default)]
   pub specifier_types: Vec<String>,
+  #[serde(default)]
   pub version_groups: Vec<version_group::AnyVersionGroup>,
 }
 
@@ -38,34 +113,9 @@ pub fn get() -> Rcfile {
   let raw_json = r#"
     {
       "customTypes": {
-        "dev": {
+        "engines": {
           "strategy": "versionsByName",
-          "path": "devDependencies"
-        },
-        "local": {
-          "strategy": "name~version",
-          "namePath": "name",
-          "path": "version"
-        },
-        "overrides": {
-          "strategy": "versionsByName",
-          "path": "overrides"
-        },
-        "peer": {
-          "strategy": "versionsByName",
-          "path": "peerDependencies"
-        },
-        "pnpmOverrides": {
-          "strategy": "versionsByName",
-          "path": "pnpm.overrides"
-        },
-        "prod": {
-          "strategy": "versionsByName",
-          "path": "dependencies"
-        },
-        "resolutions": {
-          "strategy": "versionsByName",
-          "path": "resolutions"
+          "path": "engines"
         }
       },
       "dependencyTypes": ["**"],
--- src/strategy.rs
+++ src/strategy.rs
@@ -3,6 +3,7 @@ use std::collections::HashMap;
 use serde::Deserialize;
 
 use crate::config;
+use crate::strategy;
 
 #[derive(Debug)]
 pub struct NameAndVersionPropsStrategy {
@@ -39,45 +40,53 @@ pub enum Strategy {
 
 impl Strategy {
   pub fn from_rcfile(rcfile: &config::Rcfile) -> HashMap<String, Strategy> {
-    let mut strategies_by_name: HashMap<String, Strategy> = get_strategies_by_name();
+    let dependency_types = &rcfile.dependency_types;
+    let custom_types = &rcfile.custom_types;
+    let default_types = get_default_types();
+    let mut strategies: HashMap<String, Strategy> = HashMap::new();
+    let len = dependency_types.len();
+    let include_all = len == 0 || len == 1 && dependency_types[0] == "**";
 
-    rcfile.custom_types.iter().for_each(|(name, config)| {
-      let strategy = Strategy::from_config(name, config);
-      let name = match &strategy {
-        Strategy::NameAndVersionProps(s) => &s.name,
-        Strategy::NamedVersionString(s) => &s.name,
-        Strategy::UnnamedVersionString(s) => &s.name,
-        Strategy::VersionsByName(s) => &s.name,
-      };
-      strategies_by_name.insert(name.clone(), strategy);
+    // let mut strategies_by_name: HashMap<String, Strategy> = get_strategies_by_name();
+    // let mut starting_with_exclamation: Vec<&String> = Vec::new();
+    // let mut not_starting_with_exclamation: Vec<&String> = Vec::new();
+
+    let is_included = |dep_type: &String| -> bool {
+      if include_all {
+        return true;
+      }
+      if dependency_types.contains(dep_type) {
+        return true;
+      }
+      return dependency_types.contains(&get_negated(dep_type)) == false;
+    };
+
+    default_types.iter().for_each(|(key, value)| {
+      if is_included(key) {
+        strategies.insert(key.clone(), from_config(key, value));
+      }
     });
-    strategies_by_name
-  }
 
-  pub fn from_config(name: &String, config: &AnyStrategy) -> Strategy {
-    match config.strategy.as_str() {
-      "name~version" => Strategy::NameAndVersionProps(NameAndVersionPropsStrategy {
-        name: name.clone(),
-        path: normalize_path(config.path.clone()),
-        name_path: config
-          .name_path
-          .clone()
-          .expect("A name~version strategy must have a namePath"),
-      }),
-      "name@version" => Strategy::NamedVersionString(NamedVersionStringStrategy {
-        name: name.clone(),
-        path: normalize_path(config.path.clone()),
-      }),
-      "version" => Strategy::UnnamedVersionString(UnnamedVersionStringStrategy {
-        name: name.clone(),
-        path: normalize_path(config.path.clone()),
-      }),
-      "versionsByName" => Strategy::VersionsByName(VersionsByNameStrategy {
-        name: name.clone(),
-        path: normalize_path(config.path.clone()),
-      }),
-      _ => panic!("Unknown strategy: {}", config.strategy),
-    }
+    custom_types.iter().for_each(|(key, value)| {
+      if is_included(key) {
+        strategies.insert(key.clone(), from_config(key, value));
+      }
+    });
+
+    strategies
+
+    // rcfile.custom_types.iter().for_each(|(name, config)| {
+    //   let strategy = from_config(name, config);
+    //   let name = match &strategy {
+    //     Strategy::NameAndVersionProps(s) => &s.name,
+    //     Strategy::NamedVersionString(s) => &s.name,
+    //     Strategy::UnnamedVersionString(s) => &s.name,
+    //     Strategy::VersionsByName(s) => &s.name,
+    //   };
+    //   strategies_by_name.insert(name.clone(), strategy);
+    // });
+
+    // strategies_by_name
   }
 }
 
@@ -89,60 +98,125 @@ pub struct AnyStrategy {
   pub path: String,
 }
 
-pub fn get_strategies_by_name() -> HashMap<String, Strategy> {
-  let mut strategies_by_name: HashMap<String, Strategy> = HashMap::new();
-  strategies_by_name.insert(
-    String::from("local"),
-    Strategy::NameAndVersionProps(NameAndVersionPropsStrategy {
-      name: String::from("local"),
-      path: String::from("/version"),
-      name_path: String::from("name"),
+fn from_config(name: &String, config: &AnyStrategy) -> Strategy {
+  match config.strategy.as_str() {
+    "name~version" => Strategy::NameAndVersionProps(NameAndVersionPropsStrategy {
+      name: name.clone(),
+      path: normalize_path(config.path.clone()),
+      name_path: config
+        .name_path
+        .clone()
+        .expect("A name~version strategy must have a namePath"),
     }),
-  );
-  strategies_by_name.insert(
-    String::from("dev"),
-    Strategy::VersionsByName(VersionsByNameStrategy {
-      name: String::from("dev"),
-      path: String::from("/devDependencies"),
+    "name@version" => Strategy::NamedVersionString(NamedVersionStringStrategy {
+      name: name.clone(),
+      path: normalize_path(config.path.clone()),
     }),
-  );
-  strategies_by_name.insert(
-    String::from("overrides"),
-    Strategy::VersionsByName(VersionsByNameStrategy {
-      name: String::from("overrides"),
-      path: String::from("/overrides"),
+    "version" => Strategy::UnnamedVersionString(UnnamedVersionStringStrategy {
+      name: name.clone(),
+      path: normalize_path(config.path.clone()),
     }),
-  );
-  strategies_by_name.insert(
-    String::from("peer"),
-    Strategy::VersionsByName(VersionsByNameStrategy {
-      name: String::from("peer"),
-      path: String::from("/peerDependencies"),
+    "versionsByName" => Strategy::VersionsByName(VersionsByNameStrategy {
+      name: name.clone(),
+      path: normalize_path(config.path.clone()),
     }),
-  );
-  strategies_by_name.insert(
-    String::from("pnpmOverrides"),
-    Strategy::VersionsByName(VersionsByNameStrategy {
-      name: String::from("pnpmOverrides"),
-      path: String::from("/pnpm/overrides"),
-    }),
-  );
-  strategies_by_name.insert(
-    String::from("prod"),
-    Strategy::VersionsByName(VersionsByNameStrategy {
-      name: String::from("prod"),
-      path: String::from("/dependencies"),
-    }),
-  );
-  strategies_by_name.insert(
-    String::from("resolutions"),
-    Strategy::VersionsByName(VersionsByNameStrategy {
-      name: String::from("resolutions"),
-      path: String::from("/resolutions"),
-    }),
-  );
-  strategies_by_name
+    _ => panic!("Unknown strategy: {}", config.strategy),
+  }
 }
+
+fn get_default_types() -> HashMap<String, strategy::AnyStrategy> {
+  serde_json::from_str(
+    r#"
+    {
+      "dev": {
+        "strategy": "versionsByName",
+        "path": "devDependencies"
+      },
+      "local": {
+        "strategy": "name~version",
+        "namePath": "name",
+        "path": "version"
+      },
+      "overrides": {
+        "strategy": "versionsByName",
+        "path": "overrides"
+      },
+      "peer": {
+        "strategy": "versionsByName",
+        "path": "peerDependencies"
+      },
+      "pnpmOverrides": {
+        "strategy": "versionsByName",
+        "path": "pnpm.overrides"
+      },
+      "prod": {
+        "strategy": "versionsByName",
+        "path": "dependencies"
+      },
+      "resolutions": {
+        "strategy": "versionsByName",
+        "path": "resolutions"
+      }
+    }
+    "#,
+  )
+  .unwrap()
+}
+
+// pub fn get_strategies_by_name() -> HashMap<String, Strategy> {
+//   let mut strategies_by_name: HashMap<String, Strategy> = HashMap::new();
+//   strategies_by_name.insert(
+//     String::from("local"),
+//     Strategy::NameAndVersionProps(NameAndVersionPropsStrategy {
+//       name: String::from("local"),
+//       path: String::from("/version"),
+//       name_path: String::from("name"),
+//     }),
+//   );
+//   strategies_by_name.insert(
+//     String::from("dev"),
+//     Strategy::VersionsByName(VersionsByNameStrategy {
+//       name: String::from("dev"),
+//       path: String::from("/devDependencies"),
+//     }),
+//   );
+//   strategies_by_name.insert(
+//     String::from("overrides"),
+//     Strategy::VersionsByName(VersionsByNameStrategy {
+//       name: String::from("overrides"),
+//       path: String::from("/overrides"),
+//     }),
+//   );
+//   strategies_by_name.insert(
+//     String::from("peer"),
+//     Strategy::VersionsByName(VersionsByNameStrategy {
+//       name: String::from("peer"),
+//       path: String::from("/peerDependencies"),
+//     }),
+//   );
+//   strategies_by_name.insert(
+//     String::from("pnpmOverrides"),
+//     Strategy::VersionsByName(VersionsByNameStrategy {
+//       name: String::from("pnpmOverrides"),
+//       path: String::from("/pnpm/overrides"),
+//     }),
+//   );
+//   strategies_by_name.insert(
+//     String::from("prod"),
+//     Strategy::VersionsByName(VersionsByNameStrategy {
+//       name: String::from("prod"),
+//       path: String::from("/dependencies"),
+//     }),
+//   );
+//   strategies_by_name.insert(
+//     String::from("resolutions"),
+//     Strategy::VersionsByName(VersionsByNameStrategy {
+//       name: String::from("resolutions"),
+//       path: String::from("/resolutions"),
+//     }),
+//   );
+//   strategies_by_name
+// }
 
 /// Adds a forward slash to the start of the String and replaces every "."
 /// inside the String with a "/"
@@ -150,4 +224,11 @@ fn normalize_path(path: String) -> String {
   let mut normalized_path = String::from("/");
   normalized_path.push_str(&path.replace(".", "/"));
   normalized_path
+}
+
+/// Adds "!" to the start of the String
+fn get_negated(path: &String) -> String {
+  let mut negated_path = String::from("!");
+  negated_path.push_str(&path);
+  negated_path
 }
