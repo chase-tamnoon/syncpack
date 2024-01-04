4233bec79c39a66ba70852c2c44ac8af02bcc391
Jamie Mason <jamie@foldleft.io>
2024-01-04 23:49:07 +0000

    [WIP]

--- src/instance.rs
+++ src/instance.rs
@@ -3,23 +3,23 @@ use crate::specifier::SpecifierType;
 
 #[derive(Debug)]
 pub struct Instance {
+  /// The dependency type to use to read/write this instance
+  pub dependency_type: DependencyType,
   /// The dependency name eg. "react", "react-dom"
   pub name: String,
   /// The parsed dependency specifier
   pub specifier_type: SpecifierType,
   /// The raw dependency specifier eg. "16.8.0", "^16.8.0"
   pub specifier: String,
-  /// The strategy to use for this instance
-  pub strategy: DependencyType,
 }
 
 impl Instance {
-  pub fn new(name: String, specifier: String, strategy: DependencyType) -> Instance {
+  pub fn new(name: String, specifier: String, dependency_type: DependencyType) -> Instance {
     Instance {
+      dependency_type,
       name,
       specifier_type: SpecifierType::new(specifier.as_str()),
       specifier,
-      strategy,
     }
   }
 }
