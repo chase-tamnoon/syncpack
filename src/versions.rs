1dc5b95b4656fe0fcd0a1f1ae9b6a81219c8e193
Jamie Mason <jamie@foldleft.io>
2024-05-04 11:38:56 +0100

    [WIP]

--- src/versions.rs
+++ src/versions.rs
@@ -3,11 +3,22 @@ use log::debug;
 use crate::config;
 use crate::package_json;
 use crate::version_group::VersionGroup;
+use crate::version_group::VersionGroupVariant;
 
 /// Check whether all versions are according to config
 /// Returns true if all are valid
 pub fn is_valid(version_groups: &Vec<VersionGroup>) -> bool {
-  debug!("@TODO lint version mismatches");
+  version_groups.iter().for_each(|version_group| {
+    println!("== {}", &version_group.selector.label);
+    if matches!(&version_group.variant, VersionGroupVariant::Standard) {
+      version_group
+        .instance_groups_by_name
+        .iter()
+        .for_each(|(name, instance_group)| {
+          println!("{} {:?}", name, instance_group.expected_version);
+        });
+    }
+  });
   false
 }
 
