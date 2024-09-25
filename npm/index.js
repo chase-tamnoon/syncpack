28b709d45bedfed5d70aa95c66f26a0dc35b0bc6
Jamie Mason <jamie@foldleft.io>
2024-09-25 14:52:18 +0100

    [WIP] refactor node runner

--- npm/index.js
+++ npm/index.js
@@ -2,12 +2,14 @@
 
 const { spawnSync } = require("child_process");
 
-const args = process.argv.slice(2);
 const arch = process.arch;
-const [os, extension] = ["win32", "cygwin"].includes(process.platform) ?
-  ["windows", ".exe"] : [process.platform, ""];
-const optionalDep = `syncpack-${os}-${arch}`
+const [os, extension] = ["win32", "cygwin"].includes(process.platform)
+  ? ["windows", ".exe"]
+  : [process.platform, ""];
+const optionalDep = `syncpack-${os}-${arch}`;
 const pkgSpecifier = `${optionalDep}/bin/syncpack${extension}`;
+
+const args = process.argv.slice(2);
 const processResult = spawnSync(getPathToBinary(), args, { stdio: "inherit" });
 
 process.exit(processResult.status ?? 0);
@@ -15,9 +17,12 @@ process.exit(processResult.status ?? 0);
 function getPathToBinary() {
   try {
     return require.resolve(pkgSpecifier);
-  } catch (e) {
-    throw new Error(
-      `syncpack can't find its Rust binary it expected to be installed as an optionalDependency called "${optionalDep}", the Rust binary should be at ${pkgSpecifier}`,
+  } catch (_) {
+    console.error(
+      "\x1b[31m%s\n%s\x1b[0m",
+      `! syncpack expected an optionalDependency "${optionalDep}" to be installed, containing a Rust binary at "${pkgSpecifier}"`,
+      "  Please visit https://github.com/JamieMason/syncpack/issues",
     );
+    process.exit(1);
   }
 }
